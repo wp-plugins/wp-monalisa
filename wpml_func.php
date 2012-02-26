9a0464a9f1318cb112d6a974672a659da9a1e2bc
tuxlog <tuxlog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-26 07:35:13 +0000

    wp-monalisa v1.8 tag
    git-svn-id: https://plugins.svn.wordpress.org/wp-monalisa/tags/1.8@510644 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpml_func.php
+++ wpml_func.php
@@ -1,7 +1,7 @@
 <?php
 /* This file is part of the wp-monalisa plugin for wordpress */
 
-/*  Copyright 2009  Hans Matzen  (email : webmaster at tuxlog dot de)
+/*  Copyright 2009-2011  Hans Matzen  (email : webmaster at tuxlog dot de)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -29,7 +29,22 @@ if ( !function_exists('admin_message') )
 	echo "</strong></p></div>\n";
     }
 }
-
+ 
+//
+// enqueue the wp-monalisa stylesheet individual or default
+// if no individual exists
+//
+function wpml_css() 
+{
+	$def  = "wp-monalisa-default.css";
+	$user = "wp-monalisa.css";
+	
+	if (file_exists( WP_PLUGIN_DIR . "/wp-monalisa/" . $user))
+	    $def =$user;
+	
+	wp_enqueue_style("wp-monalisa", plugins_url( $def, __FILE__));
+}
+    
 //
 // this function compares the length of k1 and k2 and returns
 // 0 if equal, 1 if k1 shorter than k2, -1 if k1 is longer than k2
